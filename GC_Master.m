3ed82e5f80edd4ad51dd946e38e836546da24e8d
jcorvino <hmpjoe@gmail.com>
2017-04-15 16:29:08 -0700

    Add files via upload

--- GC_Master.m
+++ GC_Master.m
@@ -3,10 +3,20 @@
 % Times everything
 % Runs GUI
 
+% Start afresh :)
+clear all
+close all
+clc
+
+% Verifty versions of each subroutine
 [version_flag, error_message ] = GC_Version();
 
-
-if version_flag
+% Stop GC_Master if versions are not up-to-date
+if ~version_flag
     display(error_message)
     return
 end
+
+
+
+display('GC_Master Sucessful')
