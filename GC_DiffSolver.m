0cbb190a5fb49c854815f9a3db787a55250922d0
jcorvino <hmpjoe@gmail.com>
2017-05-08 15:09:40 -0700

    Add files via upload

--- GC_DiffSolver.m
+++ GC_DiffSolver.m
@@ -24,18 +24,18 @@ eps = y(2:end) - y(1:end-1);
 del = x(2:end) - x(1:end-1);
 
 % Make D, S, Sigma_A, phi
-D       = 10*ones(n,m);
-sigma = 2*ones(n,m);
-S       = 10*ones(n,m);
+D       = 10*ones(n-1,m-1);
+sigma = 2*ones(n-1,m-1);
+S       = 10*ones(n-1,m-1);
 
 %% Pad S, sigma, and V matrices w/ zeros
 %Pad S with zeros to account for edge cases (boundaries + 4 corners)
 S_expand = zeros(n+1,m+1);
-S_expand(1:end-1,1:end-1) = S;
+S_expand(2:end-1,2:end-1) = S;
 
 %Pad sigma with zeros to account for edge cases (boundaries + 4 corners)
 Sig_expand = zeros(n+1,m+1);
-Sig_expand(1:end-1,1:end-1) = sigma;
+Sig_expand(2:end-1,2:end-1) = sigma;
 
 % Create V (pad with zeros to account for edge cases)
 V = zeros(n+1,m+1);
@@ -53,7 +53,7 @@ S_B = S_expand(2:end,1:end-1).*V(2:end,1:end-1);
 S_C = S_expand(1:end-1,2:end).*V(1:end-1,2:end);
 S_D = S_expand(2:end,2:end).*V(2:end,2:end);
 S_avg = S_A + S_B + S_C + S_D;
-phi = S_avg;
+
 
 
 
@@ -69,6 +69,40 @@ sigma_avg = Sig_A + Sig_B + Sig_C + Sig_D;
 
 
 %% Create A matrix (solves A*phi = S_avg)
+
+% Pad del and eps with a leading & trailing zero
+del = [0,del,0];
+eps = [0,eps,0];
+
+% Pad D with zeros
+D_expand = zeros(n+1,m+1);
+D_expand(2:end-1,2:end-1) = D;
+
+%% Calculate A_B (bottom)
+A_B = -diag(...
+    reshape(...
+    (D_expand(1:end-1,1:end-1).*del(1:end-1) + D_expand(2:end,2:end).*del(2:end))./(2*eps(1:end-1))...
+    ,[],1));
+% convert inf / nan values to zero
+index = isinf(A_B) | isnan(A_B);
+A_B(index) = 0;
+
+
+%% Calculate A_T (top)
+A_T = -diag(...
+    reshape(...
+    (D_expand(1:end-1,1:end-1).*del(1:end-1) + D_expand(2:end,2:end).*del(2:end))./(2*eps(1:end-1))...
+    ,[],1));
+% convert inf / nan values to zero
+index = isinf(A_T) | isnan(A_T);
+A_T(index) = 0;
+
+
+
+
+%% Calculate A_C (center)
+A_C = diag(sigma_avg(:)) - A_B - A_T
+%A = A_C
 A = eye(n*m);
 
 
