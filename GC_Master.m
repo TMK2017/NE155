64336ff22bbd650b5b2a8088af8596f91a090862
jcorvino <hmpjoe@gmail.com>
2017-05-09 16:24:22 -0700

    Significant Updates to GC_Master.m
    Added more subroutines and changed required problem inputs. It also now outputs a surface plot and excel file.

--- GC_Master.m
+++ GC_Master.m
@@ -1,22 +1,43 @@
-%% Green-Corvino Master script
+function [] = GC_Master(userx,usery,userD,userS,usersigma,filename)
+%% Green-Corvino Master Function
 % Controls Everything
 % Times everything
 % Runs GUI
+tic; % start timer
+v = 'v4.0';
 
-% Start afresh :)
-clear all
+% close figure windows
 close all
-clc
 
-% Verifty versions of each subroutine
-[version_flag, error_message ] = GC_Version();
 
-% Stop GC_Master if versions are not up-to-date
-if ~version_flag
-    display(error_message)
-    return
+
+%Write code name, version number, author names, date and time of execution
+version_message = GC_VersionData(v);
+[D,S,sigma,x,y,flag] = GC_InputData(userx,usery,userD,userS,usersigma);
+if flag
+   error('Input data is incorrectly formatted and did not pass GC_InputData.m check') 
 end
+% Print inputs to file
+GC_InputEcho(D,S,sigma,x,y, filename, version_message)
 
+% Solve for phi
+phi = GC_DiffSolver(x,y,D,sigma,S);
 
+% plot results
+figure
+hold on
+grid on
+view(3)
+[X,Y]= meshgrid(x,y);
+surf(X,Y,(phi)','EdgeColor','None')
+xlabel('X')
+ylabel('Y')
+zlabel('\phi(x,y)')
 
-display('GC_Master Sucessful')
+% print to output file
+warning('off','all')
+xlswrite(filename,phi','phi')
+warning('on','all')
+
+toc
+end
